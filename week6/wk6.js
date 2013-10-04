a4925a547c69d5a7f283934b794d3b87b5c0de94
Jose Galdamez <galdamez@mac.com>
2013-10-04 10:56:42 -0400

    More documentation

--- week6/wk6.js
+++ week6/wk6.js
@@ -1,10 +1,11 @@
-// wk6.js
+// GLOBAL VARIABLES
 
 // Up until now we've been creating all variables as global variables.
 // Generally speaking, this is bad practice because global variables can
 // be overwritten by unsuspecting code. They are also difficult to maintain.
 // If all your variables are global you are likely to run into a "spaghetti code"
-// situation.
+// situation where you reference a variable and you have no clue where it was
+// defined.
 
 var myGlobal;
 
@@ -35,6 +36,21 @@ globalFunction();
 
 // console.log(localVar);
 
+
+
+// ANONYMOUS FUNCTIONS
+
+// An anonymous function is essentially a function definition without a name.
+// The code block below is commented out because it throws a JavaScript if left as is.
+// While it may not make sense to have a function without a name you'll soon see
+// why and how they're used.
+
+// function () {
+// 	console.log('I don\'t have a name!');
+// }
+
+
+
 // FUNCTIONS AS EXPRESSIONS
 
 // A basic function will look like this
@@ -49,50 +65,83 @@ var getRandomInt = function(rangeLength) {
 	return Math.floor(Math.random() * rangeLength);
 }
 
-// See the difference?
+// Do you see the difference? The second definition of getRandomInt assigns the variable
+// to an anonymous function on the right.
 
-// The click handler below
+// The click handler below is another example if when you may want to use an anonymous
+// function. The property "onclick" is set to function that will execute when the
+// click happens.
 
 document.getElementById('clickThis').onclick = function () {
 	console.log(myGlobal, this.id);
 }
 
+
+
+// ANONYMOUS FUNCTIONS AS ARGUMENTS
+
+// The function definition below demonstrates that functions can be passed as arguments
+// to functions. While this may seem contrived in practice it is used quite often.
+// Libraries like jQuery and even some native JavaScript functions expect you to pass
+// functions as arguments.
+
 var gizmo = function (gremlin1, gremlin2, globalTest) {
+	// We're definining a local variable named myGlobal. The point is to see whether
+	// or not the code in globalTest() will access the local myGlobal or the global
+	// myGlobal.
+
 	var myGlobal;
 
 	myGlobal = 'hahaha';
 
+	// The following lines execute the functions that were passed as arguments.
+
 	gremlin1();
 	gremlin2();
 
-	// Which myGlobal value will it console.log access?
+	// Which myGlobal value will console.log access?
 	// The global myGlobal or the local myGlobal? The answer may surprise you.
 	globalTest();
 }
 
+// We call the function gizmo by passing it three arguments. All three arguments are functions.
+// We use white space to break up the arguments on to separate lines for readability's sake.
 gizmo(
 	function () {console.log('I am Stripe!');},
 	function () {console.log('I am Poker Player!');},
 	function () {console.log(myGlobal);}
 );
 
+// setTimeout will execute code after a certain period of time. It takes two arguments:
+// 1) a function,
+// 2) the number of milliseconds to wait
+// setTimeout has a cousin named setInterval which executes repeatedly. Its syntax is
+// almost identical.
+
 setTimeout(function () {
 	console.log('Thank you for your patience!');
 }, 2000);
 
-var student = {};
 
-student.name = 'Jane Doe';
-student.id = 0193013;
-student.age = 18;
-student.gender = 'F';
-student.phone = '(240) 555-5555';
-student['status'] = 'Freshman';
-student['007'] = 'James Bond';
-student.getStatus = function () {
+
+// OBJECTS
+
+// Creating an empty object
+var student1 = {};
+
+// Populating the object one property at a time.
+student1.name = 'Jane Doe';
+student1.id = 0193013;
+student1.age = 18;
+student1.gender = 'F';
+student1.phone = '(240) 555-5555';
+student1['status'] = 'Freshman';
+student1['007'] = 'James Bond';
+student1.getStatus = function () {
 	return this.status;
 }
 
+// Populating an object using an object literal
 var student2 = {
 	name : 'John Doe',
 	id : 04939048,
@@ -105,12 +154,19 @@ var student2 = {
 	}
 };
 
+// This function will manipulate a student object passed to it
 function graduate(student) {
 	student.status = 'Party time!';
 	console.log(student);
 	return student;
 }
 
+// Execute function by sending object variables
+
+graduate(student1);
+graduate(student2);
+
+// Execute by sending an object literal as the first argument
 graduate({
 	name : 'John Doe',
 	id : 04939048,
