43eec7568a1067b5cb80ae11d94424ab21abbb10
Jose Galdamez <mcstudent@jose-galdamezs-computer.local>
2013-10-26 18:24:51 -0400

    Improved comments

--- week9/wk9.js
+++ week9/wk9.js
@@ -11,6 +11,8 @@ document.getElementById('w3cExample').onclick = function() {
 	mask = document.createElement('div');
 	mask.setAttribute('id', 'mask');
 	mask.setAttribute('style', 'display: block;');
+
+	// Append mask node to the page
 	document.body.appendChild(mask);
 
 	// Create modal
@@ -18,27 +20,33 @@ document.getElementById('w3cExample').onclick = function() {
 	modal.setAttribute('id', 'modal');
 	modal.setAttribute('style', 'display: block;');
 
-	// Attach text node
+	// Attach text node to the modal node
 	modal.appendChild(textNode);
 
-	// Append to page
+	// Append node to page
 	document.body.appendChild(modal);
-}
+};
 
 // Inject onto page using innerHTML
 document.getElementById('innerHTMLExample').onclick = function() {
 	var modal, mask, text, textNode, container;
 
+	// Create text node
 	text = 'Lorem ipsum there is some text here';
 	textNode = document.createTextNode(text);
 
+	// Find a parent container to insert into
 	container = document.getElementById('container');
+
+	// Change the inner HTML of that container node
 	container.innerHTML += '<div id="mask"></div>';
 	container.innerHTML += '<div id="modal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, repudiandae, minima, in iure eos dolorum placeat consequatur vero magni eius explicabo magnam delectus laboriosam deleniti quia illo non voluptatibus libero.</div>';
 };
 
 //jQuery approach
 $('#jQueryExample').on('click', function() {
+	// Locate the parent container by way of a CSS selector
+	// Afterwards, append each HTML snippet one by one
 	$('#container')
 		.append('<div id="mask"></div>')
 		.append('<div id="modal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, repudiandae, minima, in iure eos dolorum placeat consequatur vero magni eius explicabo magnam delectus laboriosam deleniti quia illo non voluptatibus libero.</div>');
